217727fdebfdd86ddd327db1c14d547e66cb0df5
Walter Tlatenchi <walter.tlatenchi@gmail.com>
2020-02-16 10:03:05 -0800

    show after after photo capture

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "http://10.3.19.74:8080/ccapi/ver100/";
+const BASE_URL = "http://192.168.43.174:8080/ccapi/ver100/";
 
 const api = axios.create({
-  baseURL: BASE_URL,
-  timeout: 1000
+  baseURL: BASE_URL
+  // timeout: 8000
 });
 
 const capturePhoto = () => {
@@ -13,7 +13,7 @@ const capturePhoto = () => {
       af: true
     })
     .then(response => {
-      return response;
+      return getLastImage();
     })
     .catch(error => {
       console.warn(error);
@@ -31,4 +31,39 @@ const getAllImages = () => {
     });
 };
 
-export { capturePhoto, getAllImages };
+const getLastImage = () => {
+  return api
+    .get("contents/sd/100CANON/")
+    .then(response => {
+      const images = response.data.url;
+      const imagesLength = images.length;
+      return images[imagesLength - 1];
+    })
+    .catch(error => {
+      console.warn(error);
+    });
+};
+
+const startLiveFeed = () => {
+  return api
+    .post("shooting/liveview/", {
+      liveviewsize: "small",
+      cameradisplay: "on"
+    })
+    .then(response => {
+      console.log(response);
+      return response;
+    })
+    .catch(error => {
+      console.warn(error);
+    });
+};
+
+const getLiveFeed = () => {
+  return api.get("/shooting/liveview/flip").then(result => {
+    console.log(result);
+    return result;
+  });
+};
+
+export { capturePhoto, getAllImages, startLiveFeed, getLiveFeed, getLastImage };
