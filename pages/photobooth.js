0149a57dc044fbcc550e57efa516db547535ebff
Maurice Ngo <igotmauxy@gmail.com>
2020-02-16 07:35:37 -0800

    updated photo booth

--- pages/photobooth.js
+++ pages/photobooth.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { capturePhoto } from "../src/utils";
+
 class PhotoBooth extends React.Component {
   state = {
-    transcript: "hello"
+    transcript: "hello",
+    numPhotos: null,
+    srStart: false,
+    showDirections: false,
+    readyToShoot: false,
+    done: false
   };
+
   initSpeechRecognition = () => {
     const sr = new webkitSpeechRecognition();
     sr.continuous = true;
     sr.interimResults = true;
     sr.lang = "en-IN";
     sr.start();
+    this.setState({
+      srStart: true
+    });
     return sr;
   };
+
   checkCommand = ts => (commands, fn) => {
     commands.forEach(command => {
       if (ts.includes(command)) {
@@ -19,68 +30,123 @@ class PhotoBooth extends React.Component {
       }
     });
   };
+  setPhotos = num => {
+    if (this.state.numPhotos === null) {
+      this.setState({
+        numPhotos: num
+      });
+      setTimeout(() => {
+        this.setState({ showDirections: true });
+      }, 1000);
+    }
+  };
+  checkCommands = () => {
+    const commands = this.checkCommand(this.state.transcript);
+    commands(["1", "one", "what"], () => {
+      this.setPhotos(1);
+    });
+    commands(["2", "two", "too"], () => {
+      this.setPhotos(2);
+    });
+    commands(["3", "three"], () => {
+      this.setPhotos(3);
+    });
+    commands(["4", "four", "for"], () => {
+      this.setPhotos(4);
+    });
+    commands(
+      ["cheese", "she's", "cheap", "chief", "cheats", "sheetz", "chase"],
+      () => {
+        capturePhoto();
+        if (this.state.numPhotos > 0) {
+          this.setState(prevState => ({
+            numPhotos: prevState.numPhotos - 1
+          }));
+        } else {
+          this.setState({
+            done: true
+          });
+        }
+      }
+    );
+  };
+  setToReady = () => {
+    this.setState({ readyToShoot: true });
+  };
+  debounce = (callback, time) => {
+    let interval;
+    return (...args) => {
+      clearTimeout(interval);
+      interval = setTimeout(() => {
+        interval = null;
+        callback(...args);
+      }, time);
+    };
+  };
   startListening = () => {
-    capturePhoto();
     const hasSpeechRecognition = "webkitSpeechRecognition" in window;
     if (hasSpeechRecognition) {
       const sr = this.initSpeechRecognition();
-      let finalTranscripts = "";
-      sr.onresult = ({ resultIndex, results }) => {
-        let interimTranscripts = "";
-        finalTranscripts = "";
 
+      const transcribe = ({ resultIndex, results }) => {
+        let interimTranscripts = "";
         for (var i = resultIndex; i < results.length; i++) {
-          const transcript = results[i][0].transcript.toLowerCase();
-          const commands = this.checkCommand(transcript);
-          if (results[i].isFinal) {
-            commands(["1", "one", "what"], () => {
-              alert("one photo");
-            });
-            commands(["2", "two", "too"], () => {
-              alert("two photos");
-            });
-            commands(["3", "three"], () => {
-              alert("three photos");
-            });
-            commands(["4", "four", "for"], () => {
-              alert("four photo");
-            });
-            commands(
-              [
-                "cheese",
-                "she's",
-                "cheap",
-                "chief",
-                "cheats",
-                "sheetz",
-                "chase"
-              ],
-              () => {
-                capturePhoto();
-              }
-            );
-          } else {
-            interimTranscripts += transcript;
+          if (!results[i].isFinal) {
+            interimTranscripts += results[i][0].transcript.toLowerCase();
           }
         }
+        console.log(interimTranscripts);
         this.setState({
-          transcript: finalTranscripts + interimTranscripts
+          transcript: interimTranscripts
         });
+        this.checkCommands();
       };
+
+      sr.onresult = this.debounce(transcribe, 500);
       sr.onerror = function(event) {};
     }
   };
+
   render() {
     return (
       <div className="photo-booth">
-        <button className="start-button" onClick={this.startListening}>
-          Touch
-          <br />
-          To start
-          <br />
-          {/* {this.state.transcript} */}
-        </button>
-
+        {!this.state.srStart && (
+          <button className="start-button" onClick={this.startListening}>
+            Touch
+            <br />
+            To start
+          </button>
+        )}
+        {this.state.srStart && !this.state.showDirections && (
+          <div className="num-photo">
+            How many photos
+            <br />
+            do you want to take?
+            <br />
+            <div className="current-photo-num">{this.state.numPhotos}</div>
+            <div className="photo-max">(Four photos max)</div>
+          </div>
+        )}
+        {this.state.showDirections && !this.state.done && (
+          <div className="directions">
+            <div className="directions-title">Directions</div>
+            1. Pose
+            <br />
+            2. Look at the Camera
+            <br />
+            3. Say "Cheese" to take a picture
+            {/* <div>
+              {!this.state.readyToShoot && (
+                <button className="ready-button" onClick={this.setToReady}>
+                  Ready
+                </button>
+              )}
+            </div> */}
+            <div className="photos-left">
+              {this.state.numPhotos} photos left
+            </div>
+          </div>
+        )}
         <style jsx>{`
           .photo-booth {
             position: fixed;
@@ -95,9 +161,68 @@ class PhotoBooth extends React.Component {
             width: 100%;
             height: 100%;
             font-size: calc(
-              50px + (300 - 50) * ((100vw - 300px) / (1600 - 300))
+              50px + (200 - 50) * ((100vw - 300px) / (1600 - 300))
             );
             letter-spacing: 0.05em;
+            background: #eee;
+            font-weight: bold;
+          }
+          .num-photo {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            flex-direction: column;
+            font-weight: bold;
+            font-size: calc(
+              24px + (100 - 24) * ((100vw - 300px) / (1600 - 300))
+            );
+          }
+          .photo-max,
+          .photos-left {
+            margin-top: 0.7em;
+            color: #717171;
+            font-size: calc(
+              12px + (50 - 12) * ((100vw - 300px) / (1600 - 300))
+            );
+          }
+          .current-photo-num {
+            font-size: calc(
+              50px + (200 - 50) * ((100vw - 300px) / (1600 - 300))
+            );
+          }
+
+          .directions {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            align-items: flex-start;
+            justify-content: center;
+            flex-direction: column;
+            font-size: calc(
+              18px + (70 - 18) * ((100vw - 300px) / (1600 - 300))
+            );
+            padding: 10vw;
+            line-height: 2em;
+            text-align: left;
+          }
+          .directions-title {
+            font-weight: bold;
+            font-size: calc(
+              24px + (100 - 24) * ((100vw - 300px) / (1600 - 300))
+            );
+          }
+          .ready-button {
+            background: #6200ee;
+            color: #fff;
+            border-radius: 5px;
+            text-transform: uppercase;
+            padding: 0.4em 2em;
+            font-size: calc(
+              12px + (40 - 12) * ((100vw - 300px) / (1600 - 300))
+            );
           }
         `}</style>
       </div>
--- src/theme.js
+++ src/theme.js
@@ -1,36 +1,36 @@
-import { createMuiTheme } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import { createMuiTheme } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
 
 // Create a theme instance.
 const theme = createMuiTheme({
   typography: {
     fontFamily: [
-      '"Bebas Neue"',
-      'BlinkMacSystemFont',
+      // '"Bebas Neue"',
+      "BlinkMacSystemFont",
       '"Segoe UI"',
-      'Roboto',
+      "Roboto",
       '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
+      "Arial",
+      "sans-serif",
       '"Apple Color Emoji"',
       '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
+      '"Segoe UI Symbol"'
+    ].join(",")
   },
   palette: {
     primary: {
-      main: '#556cd6',
+      main: "#556cd6"
     },
     secondary: {
-      main: '#19857b',
+      main: "#19857b"
     },
     error: {
-      main: red.A400,
+      main: red.A400
     },
     background: {
-      default: '#fff',
-    },
-  },
+      default: "#fff"
+    }
+  }
 });
 
 export default theme;
